7233c1cb9a057ab37d06033aa443e2f91aa43207
sofien1230 <68479635+sofien1230@users.noreply.github.com>
2023-08-04 13:17:12 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,1 +1,10 @@
-//create web server
+// Create web server for comment
+
+//import module
+
+const express =  require('express');
+const router = express.Router();
+const commentController =  require('../controllers/comments');
+
+//Handle request
+router.get('/', commentController.comment_list);
